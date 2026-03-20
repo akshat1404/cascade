376d1b5b18ae35e7cac886ae605c1de26c12c240
akshat tiwari <akshatmtiwari@gmail.com>
2026-03-20 18:25:00 +0530

    supabase authentication happy flow tested

--- backend/main.go
+++ backend/main.go
@@ -11,8 +11,14 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 }
 
+func signInRedirectionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"Status": "Success", "Message": "You have been signed in successfully"})
+}
+
 func main() {
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/auth/callback", signInRedirectionHandler)
 	log.Println("Backend running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+  import { supabase } from "./lib/supabase";
+
+  async function signIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:8080/auth/callback",
+      },
+    });
+    if (error) console.error(error);
+  }
 </script>
 
 <main>
   <h1>Collaborent</h1>
-  <p>To Do : Google OAuth</p>
+  <button on:click={signIn}>Continue with Google</button>
 </main>
-
-<style>
-</style>
