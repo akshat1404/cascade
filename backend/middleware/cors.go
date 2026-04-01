b7b7602ede2e84ae30031071c1ce3da2209e3fe5
akshat tiwari <akshatmtiwari@gmail.com>
2026-04-01 19:31:42 +0530

    allow vercel domain

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -4,7 +4,12 @@ import "net/http"
 
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:5173" || origin == "https://collaborent.vercel.app" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		w.Header().Set("Access-Control-Max-Age", "86400")
